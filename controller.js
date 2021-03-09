8404e09e01cf1056c82085de6242e9bc4aa1cdab
Frants (pi) <>
2021-03-10 02:30:04 +0300

    mts bugfix

--- controller.js
+++ controller.js
@@ -144,11 +144,13 @@ bot.on("callback_query", async response => {
   if (JSON.parse(response.data).query_id === "mts") {
     bot.answerCallbackQuery(response.id, { text: `Обновляю данные...\nЭто может занять несколько секунд`, cache_time: 120, show_alert: true });
     const messages = await Mts.getMessagesMtsByUserId(response.message.chat.id);
-    messages.forEach(m => {
-      bot.sendMessage(m.id, m.text, m.options);
-      if (msg.from.id !== ADMIN_ID)
-        bot.sendMessage(ADMIN_ID, `Пользователю ${msg.from.first_name} ${msg.chat.id} отправлено сообщение:\n\n${m.text}`, m.options);
-    });
+    if (messages.isArray) {
+      messages.forEach(msg => {
+        bot.sendMessage(msg.id, msg.text, msg.options);
+        if (msg.from.id !== ADMIN_ID)
+          bot.sendMessage(ADMIN_ID, `Пользователю ${msg.from.first_name} ${msg.chat.id} отправлено сообщение:\n\n${msg.text}`, msg.options);
+      });
+    }
   }
 });
 
