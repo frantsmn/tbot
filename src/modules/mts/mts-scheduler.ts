c4f808d863cb07696253201402b279d56d274788
Frants Mitskun <frants.mitskun@gmail.com>
2022-05-30 11:10:10 +0300

    mts-scheduler cleanup

--- src/modules/mts/mts-scheduler.ts
+++ src/modules/mts/mts-scheduler.ts
@@ -3,48 +3,20 @@ import Logger from '../../modules/logger/logger'
 import MtsModel from './mts-model'
 
 export default class MtsScheduler {
-    BOT: any
-    MTS_FIREBASE: any
-
     constructor(BOT, MTS_FIREBASE, ADMIN_ID) {
         const logger = new Logger('mts-scheduler', BOT, ADMIN_ID);
 
         schedule.scheduleJob({hour: 5, minute: 10}, updateAccounts);
         schedule.scheduleJob({hour: 13, minute: 0}, updateAccounts);
         schedule.scheduleJob({hour: 13, minute: 10}, balanceReminders);
-        // schedule.scheduleJob('*/20 * * * *', updateUrgentAccounts);
-
-        // async function updateUrgentAccounts() {
-        //     logger.log({
-        //         value: `⌛ Проверка на наличие аккаунтов требующих обновления`,
-        //         type: 'info',
-        //     });
-        //
-        //     const userAccounts = await MTS_FIREBASE.getUrgentMtsAccounts();
-        //
-        //     if (userAccounts.length) {
-        //         logger.log({
-        //             value: `⌛ Повторное обновление аккаунтов...`,
-        //             type: 'warn',
-        //         });
-        //
-        //         await MtsModel.updateAccounts(userAccounts);
-        //         await MTS_FIREBASE.setMtsAccounts(userAccounts);
-        //
-        //         logger.log({
-        //             value: `⌛ Обновление аккаунтов завершено!`,
-        //             type: 'info',
-        //         });
-        //     }
-        // }
 
         async function updateAccounts() {
-            logger.log({
-                value: `⌛ Обновление аккаунтов...`,
-                type: 'info',
-            });
-
             try {
+                logger.log({
+                    value: `⌛ Обновление аккаунтов...`,
+                    type: 'info',
+                });
+
                 const userAccounts = await MTS_FIREBASE.getAllMtsAccounts();
                 await MtsModel.updateAccounts(userAccounts);
                 await MTS_FIREBASE.setMtsAccounts(userAccounts);
@@ -54,21 +26,21 @@ export default class MtsScheduler {
                     type: 'error',
                     isAlertAdmin: true,
                 });
+            } finally {
+                logger.log({
+                    value: `⌛ Обновление аккаунтов завершено!`,
+                    type: 'info',
+                });
             }
-
-            logger.log({
-                value: `⌛ Обновление аккаунтов завершено!`,
-                type: 'info',
-            });
         }
 
         async function balanceReminders() {
-            logger.log({
-                value: `⌛ Рассылка напоминаний о пополнении баланса...`,
-                type: 'info',
-            });
-
             try {
+                logger.log({
+                    value: `⌛ Рассылка напоминаний о пополнении баланса...`,
+                    type: 'info',
+                });
+
                 const userAccounts = await MTS_FIREBASE.getAllMtsAccounts();
                 userAccounts.forEach(account => {
                     if (account.isLowBalance)
@@ -83,13 +55,12 @@ export default class MtsScheduler {
                     type: 'error',
                     isAlertAdmin: true,
                 });
+            } finally {
+                logger.log({
+                    value: `⌛ Рассылка напоминаний о пополнении баланса завершена!`,
+                    type: 'info',
+                });
             }
-
-            logger.log({
-                value: `⌛ Рассылка напоминаний о пополнении баланса завершена!`,
-                type: 'info',
-            });
         }
     }
-
 }
