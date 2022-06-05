a525db9283c0a9b0e75269ddd913b72d09af23aa
Frants Mitskun <frants.mitskun@gmail.com>
2022-06-05 13:53:59 +0300

    +loghub router

---
+++ src/modules/logger/router.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import type winston from 'winston';
+
+export default function createRouter(logger: winston.Logger) {
+    const router = express.Router();
+
+    /**
+     * Log endpoint
+     */
+    router.get('/log', async (
+        req,
+        res,
+    ) => {
+        logger.log(req.body);
+        res.json('ok');
+    });
+
+    /**
+     * Logger status
+     */
+    router.all('/status', async (
+        req,
+        res,
+    ) => res.json('ok'));
+
+    return router;
+}
