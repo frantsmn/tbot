3ed7cd4d192103b78cfd6e89bef6ffc083d230d7
Frants Mitskun <frants.mitskun@gmail.com>
2022-06-05 23:13:15 +0300

    tips

--- src/modules/currency/index.ts
+++ src/modules/currency/index.ts
@@ -2,6 +2,10 @@ import CurrencyController from './currency-controller';
 
 export default class Currency {
     constructor(BOT) {
+        // todo: достать сюда кишки из CurrecyController
+        // todo: и инициализировать их тут
+        // todo: и в них прокинуть логгер
+
         new CurrencyController(BOT);
     }
 }
